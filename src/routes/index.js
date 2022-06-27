7aac329f35a119cc4afcedf766314396ba38739b
aldiwijayaa <m.aldiwijaya97@gmail.com>
2022-06-28 06:41:35 +0700

    first commit

---
+++ src/routes/index.js
@@ -0,0 +1,7 @@
+const router = require("express").Router();
+
+router.use("/user", require("./user"));
+router.use("/transactions", require("./transaction"));
+router.use("/profile", require("./profile"));
+
+module.exports = router;
---
+++ src/routes/transaction.js
@@ -0,0 +1,10 @@
+const transactions = require("express").Router();
+
+transactions.get("/", (req, res) => {
+  return res.json({
+    success: true,
+    message: "List all transactions",
+  });
+});
+
+module.exports = transactions;
