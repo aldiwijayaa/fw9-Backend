c8e549257774518852218e72b3b7ccb743324f4f
aldiwijayaa <m.aldiwijaya97@gmail.com>
2022-10-12 18:54:54 +0700

    updated validation

--- src/helpers/errorResponse.js
+++ src/helpers/errorResponse.js
@@ -8,25 +8,41 @@ const errorHandling = (msg, param, location = 'body') => [
   },
 ];
 
-const errorResponse = (err, res) => {
-  if (err.code === '23505' && err.detail.includes('email')) {
-    const errRes = errorHandling('Email already exist', 'email');
-    return response(res, 'Error', errRes, null, 400);
+const errorResponse = (err, result) => {
+  if(err.code==='23505'&&err.detail.includes('email')){
+    const eres = errorHandling('Email already exist', 'email');
+    return response(result, 'Error', null, eres, 400);
   }
-  if (err.code === '23505' && err.detail.includes('username')) {
-    const errRes = errorHandling('Username already exist', 'username');
-    return response(res, 'Error', errRes, null, 400);
+  if(err.code==='23505'&&err.detail.includes('username')){
+    console.log('username');
+    const eres = errorHandling('Username already exist', 'username');
+    return response(result, 'Error', null, eres, 400);
   }
-  if (err.column === 'amount' && err.message.includes('not-null')) {
-    const errRes = errorHandling('Amount can not be null', 'amount');
-    return response(res, 'Error', errRes, null, 400);
+  if(err.code==='23505'&&err.detail.includes('num_phone')){
+    const eres = errorHandling('Number Phone already used', 'num_phone');
+    return response(result, 'Error', null, eres, 400);
   }
-  if (err.code === '23503' && err.detail.includes('Key')) {
-    const errRes = errorHandling('Recepient do not exist', 'repicient_id');
-    return response(res, 'Error', errRes, null, 400);
+  if(err.code==='23505'&&err.detail.includes('user_id')){
+    const eres = errorHandling('User Id already used', 'user_id');
+    return response(result, 'Error', null, eres, 400);
   }
-  console.log(err);
-  return response(res, 'Error', null, null, 400);
+  if(err.code==='23503'&&err.detail.includes('profile')){
+    const eres = errorHandling('ID still used in profile', 'id');
+    return response(result, 'Error', null, eres, 400);
+  }if(err.code==='23503'&&err.detail.includes('sender_id')){
+    const eres = errorHandling('Id not found in tabel users', 'sender_id');
+    return response(result, 'Error', null, eres, 400);
+  }if(err.code==='23503'&&err.detail.includes('receiver_id')){
+    const eres = errorHandling('Id not found in tabel users', 'receiver_id');
+    return response(result, 'Error', null, eres, 400);
+  }if(err.code==='23503'&&err.detail.includes('transfertype')){
+    const eres = errorHandling('Id not found in tabel typeTransaction', 'transfertype');
+    return response(result, 'Error', null, eres, 400);
+  }if(err.code==='22P02'){
+    const eres = errorHandling('Wrong Input or Empty', 'user_id');
+    return response(result, 'Error', null, eres, 400);
+  }
+  return response(result, 'Error', null, null, 400);
 };
 
 module.exports = errorResponse;
